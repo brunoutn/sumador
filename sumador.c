204437826d8c49d55201a0dc85fc9c1c866bbc76
Bruno Schneider <bruno.schneider@alu.inspt.utn.edu.ar>
2024-04-17 20:25:19 -0300

    sumo tercer valor

--- sumador.c
+++ sumador.c
@@ -2,16 +2,18 @@
 
 int main() 
 {
-    int num1, num2, suma;
+    int num1, num2, num3, suma;
 
     printf("Ingrese un valor: ");
     scanf("%d", &num1);
     printf("Ingrese otro valor: ");
     scanf("%d", &num2);
+    printf("Ingrese otro valor: ");
+    scanf("%d", &num3);
 
-    suma = num1 + num2;
+    suma = num1 + num2 + num3;
 
-    printf("%d + %d = %d", num1, num2, suma);
+    printf("%d + %d + %d = %d", num1, num2, num3, suma);
 
     return 0;
 }
